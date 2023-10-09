bf6043a0ce8a6d342fcf154a3061682ce597e0dc
letwebdev <letwebdev@qq.com>
2023-10-09 12:52:26 +0800

    Complement bumpVersionNumber.sh

--- scripts/bumpVersionNumber.sh
+++ scripts/bumpVersionNumber.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Go to the root dir of the repo
+cd "$(git rev-parse --show-toplevel)" || return
 allVersionNumbers="$(git tag | grep --extended-regexp 'v[0-9]+.[0-9]+.[0-9]')"
+numberOfVersionNumbers="$(echo "${allVersionNumbers}" | wc --lines)"
 newVersionNumber="$(
 	echo "${allVersionNumbers}" | tail --lines 1 |
 		awk --field-separator=. '/[0-9]+./{$NF++;print}' OFS=.
 )"
 git tag -a "${newVersionNumber}" -m "Bump version"
+sed --in-place --regexp-extended "s/versionCode [0-9]+/versionCode ${numberOfVersionNumbers}/" ./android/app/build.gradle
+sed --in-place --regexp-extended "s/versionName \".*\"/versionName \"${newVersionNumber/v/}\"/" ./android/app/build.gradle
