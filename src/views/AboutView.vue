f9f241907bcf8395d95ae6d6dc7f8b5a5a9cae5b
letwebdev <letwebdev@qq.com>
2023-10-27 15:30:30 +0800

    Tweak

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h2>This is an about page</h2>
     <br />
-    <p><a href="https://github.com/letwebdev/hackernews">view source code</a></p>
+    <p><a href="https://github.com/letwebdev/hackernews">View source code</a></p>
     <br />
   </div>
 </template>
