588c6dee9963296a65e01d8b366e3dc22e0725f0
letwebdev <letwebdev@qq.com>
2023-10-27 17:18:36 +0800

    Fix bumpVersionNumber not bump app versionCode

--- scripts/bumpVersionNumber.sh
+++ scripts/bumpVersionNumber.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Run before build apk
 
-# Go to the root dir of the repo
-cd "$(git rev-parse --show-toplevel)" || return
-allVersionNumbers="$(git tag | grep --extended-regexp 'v[0-9]+.[0-9]+.[0-9]')"
-numberOfVersionNumbers="$(echo "${allVersionNumbers}" | wc --lines)"
+existingVersionNumbers="$(git tag | grep --extended-regexp 'v[0-9]+.[0-9]+.[0-9]')"
+
 newVersionNumber="$(
-	echo "${allVersionNumbers}" | tail --lines 1 |
+	echo "${existingVersionNumbers}" | tail --lines 1 |
 		awk --field-separator=. '/[0-9]+./{$NF++;print}' OFS=.
 )"
+
+declare -i numberOfVersionNumbers
+#  "1" stands for the newVersionNumber
+numberOfVersionNumbers=$(("$(echo "${existingVersionNumbers}" | wc --lines)" + 1))
 git tag -a "${newVersionNumber}" -m "Bump version"
 sed --in-place --regexp-extended "s/versionCode [0-9]+/versionCode ${numberOfVersionNumbers}/" ./android/app/build.gradle
 sed --in-place --regexp-extended "s/versionName \".*\"/versionName \"${newVersionNumber/v/}\"/" ./android/app/build.gradle
