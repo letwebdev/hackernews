1bd9b61284796c64c963cdfe1b1718ceb768fd49
letwebdev <letwebdev@qq.com>
2023-10-29 04:25:43 +0800

    Fix apk metadata in GitHub Action

--- scripts/setVersionNumber.sh
+++ scripts/setVersionNumber.sh
@@ -6,15 +6,29 @@ existingVersionNumbers="$(ls ./.git/refs/tags/ | grep --extended-regexp 'v[0-9]+
 declare -i numberOfVersionNumbers
 #  "1" stands for the newVersionNumber
 numberOfVersionNumbers="$(echo "${existingVersionNumbers}" | wc --lines)"
+declare existingLatestVersionNumbers
+existingLatestVersionNumbers="$(
+	echo "${existingVersionNumbers}" | tail --lines 1
+)"
+versionCode="${numberOfVersionNumbers}"
+versionNumber="${existingLatestVersionNumbers}"
 
 if [[ "$1" == "--bump" ]]; then
 	newVersionNumber="$(
-		echo "${existingVersionNumbers}" | tail --lines 1 |
+		echo "${existingLatestVersionNumbers}" |
 			awk --field-separator=. '/[0-9]+./{$NF++;print}' OFS=.
 	)"
+	versionNumber="${newVersionNumber}"
 	git tag -a "${newVersionNumber}" -m "Bump version"
 	numberOfVersionNumbers+=1
 fi
 
-sed --in-place --regexp-extended "s/versionCode [0-9]+/versionCode ${numberOfVersionNumbers}/" ./android/app/build.gradle
-sed --in-place --regexp-extended "s/versionName \".*\"/versionName \"${newVersionNumber/v/}\"/" ./android/app/build.gradle
+if [[ "${GITHUB_ACTIONS}" == true ]]; then
+	versionCode="${GITHUB_RUN_NUMBER}"
+fi
+sed --in-place --regexp-extended \
+	"s/versionCode [0-9]+/versionCode ${versionCode}/" \
+	./android/app/build.gradle
+sed --in-place --regexp-extended \
+	"s/versionName \".*\"/versionName \"${versionNumber}\"/" \
+	./android/app/build.gradle
