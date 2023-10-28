fb0f38ee8b04b8d60308d8a03200c76c99ebc8b8
letwebdev <letwebdev@qq.com>
2023-10-29 02:20:19 +0800

    Try to fix version number

--- scripts/setVersionNumber.sh
+++ scripts/setVersionNumber.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # Run before build apk
+
+# GitHub action doesn't accept `git tag`
+existingVersionNumbers="$(ls ./.git/refs/tags/ | grep --extended-regexp 'v[0-9]+.[0-9]+.[0-9]')"
+declare -i numberOfVersionNumbers
+#  "1" stands for the newVersionNumber
+numberOfVersionNumbers="$(echo "${existingVersionNumbers}" | wc --lines)"
+
 if [[ "$1" == "--bump" ]]; then
-
-	existingVersionNumbers="$(git tag | grep --extended-regexp 'v[0-9]+.[0-9]+.[0-9]')"
-
 	newVersionNumber="$(
 		echo "${existingVersionNumbers}" | tail --lines 1 |
 			awk --field-separator=. '/[0-9]+./{$NF++;print}' OFS=.
 	)"
-
-	declare -i numberOfVersionNumbers
-	#  "1" stands for the newVersionNumber
-	numberOfVersionNumbers=$(("$(echo "${existingVersionNumbers}" | wc --lines)" + 1))
 	git tag -a "${newVersionNumber}" -m "Bump version"
-	sed --in-place --regexp-extended "s/versionCode [0-9]+/versionCode ${numberOfVersionNumbers}/" ./android/app/build.gradle
-	sed --in-place --regexp-extended "s/versionName \".*\"/versionName \"${newVersionNumber/v/}\"/" ./android/app/build.gradle
+	numberOfVersionNumbers+=1
 fi
+
+sed --in-place --regexp-extended "s/versionCode [0-9]+/versionCode ${numberOfVersionNumbers}/" ./android/app/build.gradle
+sed --in-place --regexp-extended "s/versionName \".*\"/versionName \"${newVersionNumber/v/}\"/" ./android/app/build.gradle
