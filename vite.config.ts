61a96912d2ce0ccd80043cbc00b36b1fa150f474
letwebdev <letwebdev@qq.com>
2023-11-24 19:59:22 +0800

    Set up vite-plugin-electron

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,22 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vueJsx from "@vitejs/plugin-vue-jsx"
 
+import electron from "vite-plugin-electron"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    electron({
+      onstart(args) {
+        if (process.env.VITE_LAUNCHING_ELECTRON) {
+          args.startup([".", "--no-sandbox"])
+        }
+      },
+      entry: "electron/main.ts",
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
