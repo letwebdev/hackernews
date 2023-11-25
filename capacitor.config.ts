d89ab8615d15daeb20b93173b8295df2f0a24f84
letwebdev <letwebdev@qq.com>
2023-11-26 01:11:39 +0800

    Fix and refactor android workflow

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,11 @@
 import { CapacitorConfig } from "@capacitor/cli"
-
+import "dotenv/config"
 const config: CapacitorConfig = {
   appId: "github.letwebdev.hackernews",
   appName: "Hacker News",
   webDir: "dist",
   android: {
-    path: "release/android",
+    path: process.env.CAPACITOR_ANDROID_PATH,
   },
   server: {
     androidScheme: "https",
