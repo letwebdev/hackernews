07066a286fac6732d85b02f48398b546374b48f7
letwebdev <letwebdev@qq.com>
2023-12-01 04:05:21 +0800

    Be importing data to DataCharts

--- src/components/DataCharts.vue
+++ src/components/DataCharts.vue
@@ -1,29 +1,33 @@
-<template>
-  <v-chart class="chart" :option="option" />
-</template>
-
 <script setup lang="ts">
 import { storeToRefs } from "pinia"
 import { useCoreDataStore } from "@/stores/coreData"
+import { useFetchingDataStore } from "@/stores/fetchData"
 
 import { use } from "echarts/core"
 import { CanvasRenderer } from "echarts/renderers"
 import { PieChart } from "echarts/charts"
 import { TitleComponent, TooltipComponent, LegendComponent } from "echarts/components"
+import type { EChartsOption } from "echarts"
 import VChart, { THEME_KEY } from "vue-echarts"
+
 import { ref, provide } from "vue"
 import type { Ref } from "vue"
 
-import type { EChartsOption } from "echarts"
-
 // Load data
 // TODO
-const lists = storeToRefs(useCoreDataStore()).lists
-setTimeout(() => {
-  const liveDataSet = useCoreDataStore().liveDataSet
-  const test = liveDataSet[0].listName
-  console.log(`--------------------${test}`)
-}, 5000)
+const liveDataSetInitialied = useFetchingDataStore().getLiveDataSetInitializationState
+/* const coreData = useCoreDataStore() */
+const coreDataRef = storeToRefs(useCoreDataStore())
+const lists = coreDataRef.lists
+
+let interValId: number | null = window.setInterval(() => {
+  if (liveDataSetInitialied.value) {
+    if (interValId) {
+      clearInterval(interValId)
+      interValId = null
+    }
+  }
+}, 200)
 
 provide(THEME_KEY, "light")
 
@@ -31,13 +35,14 @@ use([CanvasRenderer, PieChart, TitleComponent, TooltipComponent, LegendComponent
 
 const legendData: string[] = []
 const seriesData: object[] = []
+let testValue = ref(100)
 for (const list of lists.value) {
   if (list.name === "maxitem" || list.name === "updates") {
     continue
   }
   const storyType: string = list.description
   legendData.push(storyType)
-  seriesData.push({ value: 100, name: storyType })
+  seriesData.push({ value: testValue, name: storyType })
 }
 const option: Ref<EChartsOption> = ref({
   title: {
@@ -71,7 +76,9 @@ const option: Ref<EChartsOption> = ref({
   ],
 })
 </script>
-
+<template>
+  <v-chart class="chart" :option="option" />
+</template>
 <style scoped>
 .chart {
   height: 400px;
