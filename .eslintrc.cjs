e0a02d6bcec428be0f9e98357da7a2985395b96e
letwebdev <letwebdev@qq.com>
2024-02-16 03:28:19 +0800

    chore: update eslint and prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,39 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+  extends: [
+    "eslint:recommended",
+    "airbnb-base",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "@vue/eslint-config-prettier/skip-formatting",
+    "@vue/eslint-config-typescript",
+    "plugin:vue/vue3-recommended",
   ],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "vue/no-v-html": 0,
+    "no-else-return": 0,
+    "prefer-destructuring": 0,
+    "lines-between-class-members": 0,
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    "no-console": 0,
+    "no-plusplus": 0,
+    "import/prefer-default-export": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "import/extensions": 0,
+    /**
+     * false positive reports with Vite
+     * @see https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-unresolved.md
+     */
+    "import/no-unresolved": 0,
+  },
 }
