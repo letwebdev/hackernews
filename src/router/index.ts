2a891b761f0fbdfceeffe95cab266a5418fa7285
letwebdev <letwebdev@qq.com>
2024-02-17 03:16:00 +0800

    chore: tweak

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router"
-import HackerNews from "../views/HackerNewsView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +6,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HackerNews,
+      component: () => import("@/views/HackerNewsView.vue"),
     },
     {
       path: "/about",
@@ -15,7 +14,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("@/views/AboutView.vue"),
     },
     {
       path: "/charts",
@@ -23,7 +22,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/ChartsView.vue"),
+      component: () => import("@/views/ChartsView.vue"),
     },
     {
       path: "/settings",
