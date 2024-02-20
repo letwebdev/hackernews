ae873fdace8e2cf17be55fb3665d2a5fdaa001fc
letwebdev <letwebdev@qq.com>
2024-02-20 12:34:41 +0800

    feat: update style of `ItemPost`

--- src/components/ItemPost.vue
+++ src/components/ItemPost.vue
@@ -22,18 +22,27 @@ function displayingLink(item: Item): boolean {
     <h2 v-show="settings.displayingItemTitle.value">
       <a
         :href="titleUrl(item)"
-        class="flex items-center"
+        variant="plain"
+        class="text-none"
       >
-        <div class="i-mdi-web mr-2px" />
-        {{ item.title }}</a>
+        <div class="i-mdi-web mr-2px mt-4px" />
+        {{ item.title }}
+      </a>
     </h2>
     <p
       v-if="settings.displayingItemText.value"
       v-html="item.text"
     />
-    <ul class="list-none">
+    <ul class="ml-30px list-none">
       <li v-if="displayingLink(item)">
-        link: <a :href="item.url">{{ item.url }}</a>
+        <label class="mr-10px min-w-fit"> link: </label>
+        <a
+          class="text-none whitespace-normal break-all"
+          variant="plain"
+          :href="item.url"
+        >
+          {{ item.url }}
+        </a>
       </li>
       <div class="grid grid-cols-3 max-w-700px">
         <li v-if="settings.displayingItemType.value">
@@ -41,12 +50,19 @@ function displayingLink(item: Item): boolean {
         </li>
         <li v-if="settings.displayingItemId.value">
           id:
-          <a :href="item.discuss.href">
+          <v-btn
+            class="text-none whitespace-normal !items-center"
+            variant="plain"
+            :href="item.discuss.href"
+          >
             {{ item.id }}
             <v-tooltip
               activator="parent"
               location="top"
-            >Go to discuss</v-tooltip></a>
+            >
+              Go to discuss
+            </v-tooltip>
+          </v-btn>
         </li>
         <li v-if="settings.displayingItemTime.value">
           time: {{ item.readableTime }}
@@ -59,26 +75,35 @@ function displayingLink(item: Item): boolean {
     class="mb-3px mt-2px"
   />
 </template>
-<style scoped>
+<style scoped lang="scss">
 * {
   overflow-wrap: break-word;
 }
 
 h2 {
-  color: hsl(160 100% 37% / 1);
   font-size: 110%;
   text-decoration: none;
   transition: 0.4s;
-
-  &:hover {
-    background-color: hsl(160 100% 37% / 0.2);
-    cursor: pointer;
-  }
 }
 
+a,
 li {
   /* Wrap "_", etc. in long link */
   /* break-all so that link won't in new line */
+  display: flex;
+  text-decoration: none;
   word-break: break-all;
+}
+
+li {
+  align-items: center;
+}
+
+a {
+  align-items: start;
+
+  &:hover {
+    text-decoration: underline;
+  }
 }
 </style>
