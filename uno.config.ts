cd558767a15eda7deead58767460f080caae5ecc
letwebdev <letwebdev@qq.com>
2024-02-22 05:09:18 +0800

    chore: `unocss` enable Variant group transformer

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig, presetUno, presetIcons } from "unocss"
+import transformerVariantGroup from "@unocss/transformer-variant-group"
 
 export default defineConfig({
   presets: [presetUno(), presetIcons()],
+  transformers: [transformerVariantGroup()],
 })
