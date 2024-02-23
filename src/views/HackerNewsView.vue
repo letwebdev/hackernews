b7eba8beaf0af6f4a0f5a205b5f0a1c9dc427314
letwebdev <letwebdev@qq.com>
2024-02-23 14:17:29 +0800

    fix: fix height of `HackerNewsView`

--- src/views/HackerNewsView.vue
+++ src/views/HackerNewsView.vue
@@ -7,18 +7,21 @@ import DataCharts from "@/components/DataCharts.vue"
 import { useCoreDataStore } from "@/stores/coreData"
 import { useFetchingDataStore } from "@/stores/fetchData"
 
+const liveDataCacheInitialized = useFetchingDataStore().getLiveDataCacheInitializationState
+
 const promptOfFetching = storeToRefs(useFetchingDataStore()).promptOfFetching
 const items = storeToRefs(useCoreDataStore()).items
 /* console.log(items) */
 
 const isLargeScreen = computed(() => window.matchMedia("(min-width: 2560px)").matches)
 // TODO Be able to drag controlPanel
+const mounted = ref(false)
 </script>
 <template>
   <ControlPanel class="controlPanel" />
 
   <div class="hackerNewsView mt-1%">
-    <main class="min-h-101vh">
+    <main :class="{ 'min-h-101vh': liveDataCacheInitialized }">
       <ItemPost
         v-for="item in items"
         :key="item.id"
