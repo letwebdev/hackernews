e4990b9f850f3139833a8add01414074020e206e
letwebdev <letwebdev@qq.com>
2024-02-25 12:13:50 +0800

    fix: fix unocss `breakpoint`

--- uno.config.ts
+++ uno.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
   transformers: [transformerVariantGroup()],
   theme: {
     breakpoints: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
       huge: "2560px",
     },
   },
